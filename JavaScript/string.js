1ef512b0cdd3605cb1c36059ce338702f33eed51
Sudhir Singh <sudhirsingh3923@gmail.com>
2024-09-07 10:55:33 +0530

    new commit

---
+++ JavaScript/string.js
@@ -0,0 +1,24 @@
+let msg = "    he   llo    "
+// let password = prompt("set your password");
+// console.log(password.trim());
+// console.log(password); // trim is methods which trim whitespace from left and right from the words not from the middle
+// let newPass = password.trim();
+// console.log(newPass); // string return new string by changing the string its means string is immutable in javaScript 
+
+
+let str = "Random";
+
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+
+// indexOf--> Returns the first of occurrence of some value in string. Or gives -1 if not found.
+let str1 = "iloveCoding";
+
+str1.indexOf("love");
+str1.indexOf("j");
+str1.indexOf("o");
+console.log(str1);
+console.log(str1.indexOf("love"));
+console.log(str1.indexOf("j"));
+console.log(str1.indexOf("o"));
+
