6b8934e04b9eb484467d3cfba8b8580cf1f82a22
Sudhir Singh <sudhirsingh3923@gmail.com>
2024-09-10 13:14:38 +0530

    commit

---
+++ JavaScript/Assignment2.js
@@ -0,0 +1,57 @@
+// Question 1
+// let arr = [7, 9, 0, -2];
+// let n =3;
+
+// let ans = arr.slice(0, n);
+// console.log(ans);  // [7, 9, 0]
+
+// Question 2
+
+// let arr = [7, 9, 0, -2];
+// let n = 3;
+
+// let ans = arr.slice(arr.length-n);
+// console.log(ans); // [9, 0, -2]
+
+// // Question 3
+
+// let str = prompt("please enter a string");
+
+// if (str.length == 0) {
+//     console.log("string is empty");
+    
+// } else {
+//     console.log("string is not empty");
+    
+// }
+
+// Question 4
+
+// let str = "ApNaCoLlEgE";
+// let idx = 3;
+
+// if (str[idx] == str[idx].toLowerCase()) {
+//     console.log("character is lowercase");
+// } else {
+//     console.log("character is not lowercase");   
+// }
+
+// Question 5
+
+let str = prompt("please enter a string");
+console.log(`original string = ${str}`);
+console.log(`string without spaces = ${str.trim()}`);
+
+
+// Question 6
+
+// let arr = ["hello", 'a', 23, 64, 99, -6];
+// let item = 65;
+
+// if(arr.indexOf(item) != -1) {
+//     console.log("element exists in array");
+    
+// } else {
+//     console.log("element doesn't exist in array");
+    
+// }
