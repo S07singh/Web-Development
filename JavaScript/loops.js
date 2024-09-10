05df668367a11cc6411fa4248850d00cc79f80eb
Sudhir Singh <sudhirsingh3923@gmail.com>
2024-09-10 20:10:31 +0530

    commit

---
+++ JavaScript/loops.js
@@ -0,0 +1,66 @@
+// For odd number
+// for (let i = 1; i<=15; i=i+2) {
+//     console.log(i);
+// }
+// console.log("Backward");
+
+// for (let i = 19; i>=1; i=i-2) {
+//     console.log(i);
+// }
+// console.log("even");
+
+// for( let i = 2; i <= 10; i=i+2) {
+//     console.log(i);
+// } 
+// console.log("Backward");
+// for( let i = 10; i >= 2; i=i-2) {
+//     console.log(i);
+// }
+
+/*
+infinite loop (DANGER)
+for (let i=1; i>=0; i++){
+} 
+
+for (let i=1; i<=5; i--){
+}
+
+for(let i=1; ; i++){
+}
+ 
+*/
+
+// Table 
+
+// let n = prompt("write your number");
+// n = parseInt(n); // what it will do is it convert string input into integer.
+
+// for (let i=n; i<=n*10; i = i+n) {
+//     console.log(i);    
+// }
+
+// nested loop
+
+// for (let i=1; i<=3; i++){
+//     console.log(`outer loop ${i}`);
+    
+//     for(let j=1; j<=3; j++){
+//         console.log(j);
+        
+//     }
+// }
+
+// while loop
+
+// let i = 5;
+// while (i>=1) {
+//     console.log(i);
+//     i--;
+// }
+
+let i = 0;
+while (i<=20) {
+    console.log(i);
+    i= i+2; // even
+}
+
