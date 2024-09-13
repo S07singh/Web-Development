ef723721956ace54f587deb5327cddbbac42d448
Sudhir Singh <sudhirsingh3923@gmail.com>
2024-09-13 13:28:36 +0530

    commit

---
+++ JavaScript/07_function.js
@@ -0,0 +1,37 @@
+function hello () {
+    console.log("hello");
+}
+
+function printName(){
+    console.log("apna college");
+    console.log("sudhir singh");
+}
+
+function print1to5() {
+    for (let i = 0; i <=5; i++) {
+        console.log(i);
+    }
+}
+
+function isAdult() {
+    // let age = 18;
+    let age = 13;
+    if (age>=18) {
+        console.log("Adult");
+    } else {
+        console.log("not adult");
+    }
+}
+
+// printName();
+// print1to5();
+isAdult();
+
+// Question 1
+
+function printPoem() {
+    console.log("Twinkle Twinkle, little star");
+    console.log("how i wonder what you are");
+}
+
+printPoem();
